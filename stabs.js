7073aec51998c87b171eb88de5c070da33a9c6cb
A. M. Douglas <amd@xmli5.io>
2016-03-04 06:11:15 +0000

    Update stabs.js

--- stabs.js
+++ stabs.js
@@ -4,40 +4,42 @@
       z           =tabcontainer.length;
 
   while(z--){
-    let t       =tabcontainer[z],
-        tabs    =t.querySelectorAll('.tab'),
-        a       =tabs.length,
-        tabpanes=t.querySelectorAll('.tabpane'),
-        b       =tabpanes.length;
-
-    while(a--){
-      tabs[a].addEventListener("click",function(e){
-        e.preventDefault;
-        var c=this,
-            d=Array.prototype.slice.call(c.parentNode.children),
-            f=d.length;
-
-        while(f--){
-          var j=d[f].className.split(" "),
-              v=j.indexOf("tabpane"),
-              w=j.indexOf("tab"),
-              y=j.indexOf("active");
+    (function(){
+      var t       =tabcontainer[z],
+          tabs    =t.querySelectorAll('.tab'),
+          a       =tabs.length,
+          tabpanes=t.querySelectorAll('.tabpane'),
+          b       =tabpanes.length;
   
-          if(d[f]!=c&&w!=-1){
-            d[f].className="tab";
+      while(a--){
+        tabs[a].addEventListener("click",function(e){
+          e.preventDefault;
+          var c=this,
+              d=Array.prototype.slice.call(c.parentNode.children),
+              f=d.length;
+  
+          while(f--){
+            var j=d[f].className.split(" "),
+                v=j.indexOf("tabpane"),
+                w=j.indexOf("tab"),
+                y=j.indexOf("active");
+    
+            if(d[f]!=c&&w!=-1){
+              d[f].className="tab";
+            }
+            if(d[f]!=c&&v!=-1&&y!=-1){
+              d[f].className="tabpane";
+            }
           }
-          if(d[f]!=c&&v!=-1&&y!=-1){
-            d[f].className="tabpane";
+  
+          if(c.className==="tab"||"tab active"){
+            var arrtabs=Array.prototype.slice.call(tabs),
+                idx    =arrtabs.indexOf(c);
+            c.className="tab active";
+            tabpanes[idx].className="tabpane active";
           }
-        }
-
-        if(c.className==="tab"||"tab active"){
-          var arrtabs=Array.prototype.slice.call(tabs),
-              idx    =arrtabs.indexOf(c);
-          c.className="tab active";
-          tabpanes[idx].className="tabpane active";
-        }
-      });
-    }
+        });
+      }
+    })();
   }
 })();
