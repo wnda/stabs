e17a00a4fce3cf7f66dafe4816b9bad0e6a20014
A. M. Douglas <amd@xmli5.io>
2016-04-12 14:40:19 +0100

    Update stabs.js

--- stabs.js
+++ stabs.js
@@ -1,19 +1,19 @@
 (function(){
   "use strict";
-  var tabcontainer=document.getElementsByClassName('tabcontainer'),
-      z           =tabcontainer.length;
+  var tabcontainer = document.getElementsByClassName('tabcontainer'),
+      z            = tabcontainer.length;
   
   function stabs(){
-      var t       =tabcontainer[z],
-          tabs    =t.querySelectorAll('.tab'),
-          a       =tabs.length,
-          tabpanes=t.querySelectorAll('.tabpane'),
-          b       =tabpanes.length;
+      var t        = tabcontainer[z],
+          tabs     = t.querySelectorAll('.tab'),
+          a        = tabs.length,
+          tabpanes = t.querySelectorAll('.tabpane'),
+          b        = tabpanes.length;
       
-      function getChildren(n,skipMe){
-        var r=[];
-        for ( ; n; n=n.nextSibling)
-          if(n.nodeType===1&&n!=skipMe)
+      function getChildren(n, skipMe){
+        var r = [];
+        for ( ; n; n = n.nextSibling)
+          if(n.nodeType === 1 && n != skipMe)
           r.push(n);
           return r;
       }
@@ -22,34 +22,34 @@
       }
     
       function tabclick(){
-        var c=this;
-        var d=getSiblings(c);
-        var f=d.length;
+        var c = this,
+            d = getSiblings(c),
+            f = d.length,
+            m = Array.prototype.indexOf.call(tabs, c);
   
         while(f--){
-          var j=d[f].className.split(" "),
-              v=j.indexOf("tabpane"),
-              w=j.indexOf("tab"),
-              y=j.indexOf("active");
+          var j = d[f].className.split(" "),
+              v = j.indexOf("tabpane"),
+              w = j.indexOf("tab"),
+              y = j.indexOf("active");
     
-          if(d[f]!=c&&w!=-1){
+          if (d[f] != c && w != -1){
             d[f].classList.remove("active");
           }
-          if(d[f]!=c&&v!=-1&&y!=-1){
+          if (d[f] != c && v != -1 && y != -1){
             d[f].classList.remove("active");
           }
         }
-        if(c.className==="tab"||"tab active"){
-          var idx=Array.prototype.indexOf.call(tabs,c);
+        if (c.className === "tab" || "tab active"){
           c.classList.add("active");
-          tabpanes[idx].classList.add("active");
+          tabpanes[m].classList.add("active");
         }
       }
-      while(a--){
-        tabs[a].addEventListener("click",tabclick,false);
+      while (a--){
+        tabs[a].addEventListener("click", tabclick, false);
       }
     }
-  while(z--){
+  while (z--){
     stabs();
   }
 })();
