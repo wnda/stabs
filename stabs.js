bab49565299d0937454aaeacdaeec986c3a1b8ec
A. M. Douglas <amd@xmli5.io>
2016-04-16 23:29:57 +0100

    Update stabs.js

--- stabs.js
+++ stabs.js
@@ -1,49 +1,23 @@
 (function(){
   "use strict";
-  var tabcontainer = document.getElementsByClassName('tabcontainer'),
-      z            = tabcontainer.length;
-  
+  var tabcontainer  = document.querySelectorAll(".tabcontainer"),
+      z             = tabcontainer.length;
   function stabs(){
-      var t        = tabcontainer[z],
-          tabs     = t.querySelectorAll('.tab'),
-          a        = tabs.length,
-          tabpanes = t.querySelectorAll('.tabpane'),
-          b        = tabpanes.length;
-      
-      function getChildren(n, skipMe){
-        var r = [];
-        for ( ; n; n = n.nextSibling)
-          if(n.nodeType === 1 && n != skipMe)
-          r.push(n);
-          return r;
-      }
-      function getSiblings(n){
-        return getChildren(n.parentNode.firstChild, n);
-      }
-    
-      function tabclick(){
-        var c = this,
-            d = getSiblings(c),
-            f = d.length,
-            m = Array.prototype.indexOf.call(tabs, c);
-  
-        while(f--){
-          var j = d[f].className.split(" "),
-              v = j.indexOf("tabpane"),
-              w = j.indexOf("tab"),
-              y = j.indexOf("active");
-    
-          if (d[f] != c && w != -1){
-            d[f].classList.remove("active");
-          }
-          if (d[f] != c && v != -1 && y != -1){
-            d[f].classList.remove("active");
-          }
+      var t         = tabcontainer[z],
+          tabs      = t.querySelectorAll(".tab"),
+          a         = tabs.length,
+          tabpanes  = t.querySelectorAll(".tabpane"),
+          b         = tabpanes.length;
+      function tabclick(e){
+        var c       = e.target,
+            m       = Array.prototype.indexOf.call(tabs, c),
+            actives = t.querySelectorAll(".active"),
+            d       = actives.length;
+        while (d--){
+          actives[d].classList.remove("active");
         }
-        if (c.className === "tab"){
-          c.classList.add("active");
-          tabpanes[m].classList.add("active");
-        }
+        c.classList.add("active");
+        tabpanes[m].classList.add("active");
       }
       while (a--){
         tabs[a].addEventListener("click", tabclick, false);
