b032ed8c1f07685ba68759f39b21f61ca19b72c7
A. M. Douglas <amd@xmli5.io>
2016-04-17 01:04:08 +0100

    Update stabs.js

--- stabs.js
+++ stabs.js
@@ -9,21 +9,29 @@
           tabpanes  = t.querySelectorAll(".tabpane"),
           b         = tabpanes.length;
       function tabclick(e){
-        var c       = e.target,
+        var c       = e.target || e.srcElement || window.event.target || window.event.srcElement;
             m       = Array.prototype.indexOf.call(tabs, c),
             actives = t.querySelectorAll(".active"),
             d       = actives.length;
-        while (d--){
+        while ( d-- ){
           actives[d].classList.remove("active");
         }
         c.classList.add("active");
         tabpanes[m].classList.add("active");
       }
-      while (a--){
-        tabs[a].addEventListener("click", tabclick, false);
+      while ( a-- ){
+        if ( tabs[a].addEventListener ){
+          tabs[a].addEventListener( "click", tabclick, false );
+        } 
+        else if ( tabs[a].attachEvent ){
+          tabs[a].attachEvent( "onclick", tabclick );
+        }
+        else {
+          tabs[a].onclick=tabclick;
+        }
       }
     }
-  while (z--){
+  while ( z-- ){
     stabs();
   }
 })();
