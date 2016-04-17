6cc41b00f88d3c1efbdf077aba8b82e147da3b42
A. M. Douglas <amd@xmli5.io>
2016-04-17 01:47:53 +0100

    Update stabs.js

--- stabs.js
+++ stabs.js
@@ -1,11 +1,11 @@
 (function(){
   "use strict";
   function stabs(config){
-    var tabcontainer_selector = "." + config.tabcontainer || ".tabcontainer",
-        tab_selector = "." + config.tab || ".tab",
-        tabpane_selector = "." + config.tabpane || ".tabpane",
-        active_selector = "." + config.active || ".active",
-        active_class = config.active || "active",
+    var tabcontainer_selector = ".tabcontainer" || "." + config.tabcontainer,
+        tab_selector = ".tab" || "." + config.tab,
+        tabpane_selector = ".tabpane" || "." + config.tabpane,
+        active_selector = ".active" || "." + config.active,
+        active_class = "active" || config.active,
         tabcontainers = document.querySelectorAll(tabcontainer_selector),
         z = tabcontainers.length;
     function stabsEvents(){
