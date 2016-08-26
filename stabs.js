f3ac997874911cfb4f9f3436325e2e95141b4e16
A. M. Douglas <me@amdouglas.com>
2016-08-26 12:34:15 +0100

    Update stabs.js

--- stabs.js
+++ stabs.js
@@ -1,94 +1,56 @@
-;(function(){
-  
+(function(){
   "use strict";
-  
-  function stabs(config){
-    
-    var tabcontainer_selector = config && config.tabcontainer ? "." + config.tabcontainer : ".tabcontainer",
-        tab_selector          = config && config.tab          ? "." + config.tab          : ".tab",
-        tabpane_selector      = config && config.tabpane      ? "." + config.tabpane      : ".tabpane",
-        active_selector       = config && config.active       ? "." + config.active       : ".active",
-        active_class          = config && config.active       ? config.active             : "active",
-        add_hash              = config && config.addhash      ? config.addhash            : false,
-        tabcontainers         = document.querySelectorAll(tabcontainer_selector),
-        z                     = tabcontainers.length;
-    
-    function amendCss (w,x,y){
-      
-      var p = w.split(" "),
-          q = w.length,
-          r = [];
-          
-      while (q--){
-        p[q] && p[q] != active_class && r.push(p[q]);
+  var tabcontainer=document.querySelectorAll('.tabcontainer'),
+      z           =tabcontainer.length;
+
+  function stabs(){
+      var t       =tabcontainer[z],
+          tabs    =t.querySelectorAll('.tab'),
+          a       =tabs.length,
+          tabpanes=t.querySelectorAll('.tabpane'),
+          b       =tabpanes.length;
+
+      function getChildren( n, skipMe ){
+        var r = [];
+        for ( ; n; n = n.nextSibling )
+          if ( n.nodeType == 1 && n != skipMe)
+          r.push( n );
+          return r;
       }
-      
-      y && r.push(x);
-      
-      return r.join(" ");
-    
-    }
-    
-    function stabsEvents(){
-      
-      var t        = tabcontainers[z],
-          tabs     = t.querySelectorAll(tab_selector),
-          a        = tabs.length,
-          tabpanes = t.querySelectorAll(tabpane_selector),
-          b        = tabpanes.length;
-          
-      function stabsClick(e){
-        
-        var c       = e.target || e.srcElement || window.event.target || window.event.srcElement,
-            m       = [].indexOf.call(tabs, c),
-            actives = t.querySelectorAll(active_selector),
-            d       = actives.length;
-            
-        if ("classList" in document.createElement("_")){
-          while(d--){
-            actives[d].classList.remove(active_class);
+
+      function getSiblings( n ) {
+        return getChildren( n.parentNode.firstChild, n );
+      }
+
+      function tabclick(){
+        var c=this;
+        var d=getSiblings(c);
+        var f=d.length;
+
+        while(f--){
+          var j=d[f].className.split(" "),
+              v=j.indexOf("tabpane"),
+              w=j.indexOf("tab"),
+              y=j.indexOf("active");
+
+          if(d[f]!=c&&w!=-1){
+            d[f].className="tab";
           }
-          c.classList.add(active_class);
-          tabpanes[m].classList.add(active_class);
-        } 
-        else {
-          while(d--){
-            actives[d].className = amendCss(actives[d].className, active_class, 0);
+          if(d[f]!=c&&v!=-1&&y!=-1){
+            d[f].className="tabpane";
           }
-          c.className            = amendCss(c.className, active_class, 1);
-          tabpanes[m].className  = amendCss(tabpanes[m].className, active_class, 1);
         }
-        
-        if (add_hash && c.id && location.hash !== c.id){
-          location.hash = c.id;
+        if(c.className==="tab"||"tab active"){
+          var idx=Array.prototype.indexOf.call(tabs,c);
+          c.className="tab active";
+          tabpanes[idx].className="tabpane active";
         }
-        
-        return false;
-        
       }
-      
-      while (a--){
-        
-        if (tabs[a].addEventListener){
-          tabs[a].addEventListener("click", stabsClick, false);
-        } 
-        else if (tabs[a].attachEvent){
-          tabs[a].attachEvent("onclick", stabsClick);
-        } 
-        else {
-          tabs[a].onclick=stabsClick;
-        }
-        
+      while(a--){
+        tabs[a].addEventListener("click",tabclick,false);
       }
-      
     }
-    
-    while (z--){
-      stabsEvents();
-    }
-    
+  while(z--){
+    stabs();
   }
-  
-  window.stabs = stabs;
-  
 })();
