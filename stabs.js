ea9e0ce8c8f4f3176e2a3f86bbfac011157daf65
A. M. Douglas <me@amdouglas.com>
2016-09-07 15:21:49 +0100

    Update stabs.js

--- stabs.js
+++ stabs.js
@@ -25,58 +25,54 @@
   
   var isInArray = function( arr, item ){
     var len = arr.length;
-    while( len-- ) if ( item === arr[len] ) return true;
+    while(len--){
+      if(item===arr[len]) return true;
+    }
     return false;
   };
 
-  var amendCss = function( current_classname, class_to_toggle, add_class ){
+  var amendCSS = function( current_classname, class_to_toggle, add_class ){
     var arr_classes   = current_classname.split(' '),
         len           = current_classname.length,
         l             = 0,
         lacks_class   = !isInArray( arr_classes, class_to_toggle ),
         new_classname = [];
     for( ; len > l; l++ ){ arr_classes[l] !== class_to_toggle && new_classname.push( arr_classes[l] ); }
-    !!lacks_class && !!add_class && new_classname.push( class_to_toggle );
+    lacks_class && add_class && new_classname.push( class_to_toggle );
     return (new_classname.join(' ')).trimRight();
   };
   
   function stabs(t){
-      var tabs     = t.querySelectorAll('.tab'),
-          a        = tabs.length,
-          tabpanes = t.querySelectorAll('.tabpane'),
-          b        = tabpanes.length;
+      var tabs     = t.querySelectorAll('.tab');
+      var a        = tabs.length;
+      var tabpanes = t.querySelectorAll('.tabpane');
+      var b        = tabpanes.length;
           
       function tabclick(){
         var c = this;
         var d = getSiblings(c);
         var f = d.length;
         var idx = [].indexOf.call(tabs, c);
-
         while(f--){
           var j = d[f].className.split(' '),
               v = j.indexOf('tabpane'),
               w = j.indexOf('tab'),
               y = j.indexOf('active');
-  
-          if ( d[f] !== c && w !== -1 ) d[f].className = amendCss(d[f].className,'active',false);
-          if ( d[f] !== c && v !== -1 && y !== -1 ) d[f].className = amendCss(d[f].className,'active',false);
-      
+          if ( d[f] !== c && w !== -1 ) d[f].className = amendCSS(d[f].className,'active',false);
+          if ( d[f] !== c && v !== -1 && y !== -1 ) d[f].className = amendCSS(d[f].className,'active',false);
         }
-        if (c.className === 'tab' ) c.className = amendCss(c.className,'active',true);
-        if (tabpanes[idx].className === 'tabpane') tabpanes[idx].className = amendCss(tabpanes[idx].className,'active',true);
+        if (c.className === 'tab' ) c.className = amendCSS(c.className,'active',true);
+        if (tabpanes[idx].className === 'tabpane') tabpanes[idx].className = amendCSS(tabpanes[idx].className,'active',true);
+        if ( c.id && location.hash !== c.id ) location.hash = c.id;
       }
-      
       while(a--){
         if ( 'addEventListener' in window ) tabs[a].addEventListener( 'click', tabclick, false );
         else if ( 'attachEvent' in window ) tabs[a].attachEvent( 'onclick', tabclick );
         else tabs[a].onclick = tabclick;
       }
-      
     }
-  
   var tabcontainer = document.querySelectorAll('.tabcontainer'),
       z            = tabcontainer.length;
-
   while(z--){
     stabs( tabcontainer[z] );
   }
